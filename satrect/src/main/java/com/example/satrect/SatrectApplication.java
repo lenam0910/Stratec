d13fe167c29ab6275bae1c120d02e7cb8cf27011
Nguyen19092001 <nguyen19092001@gmail.com>
2025-05-30 09:22:10 +0700

    okok

---
+++ satrect/src/main/java/com/example/satrect/SatrectApplication.java
@@ -0,0 +1,13 @@
+package com.example.satrect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatrectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatrectApplication.class, args);
+	}
+
+}
---
+++ satrect/src/test/java/com/example/satrect/SatrectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.satrect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatrectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
