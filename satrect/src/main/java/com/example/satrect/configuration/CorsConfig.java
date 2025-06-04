66eceefc9d7c01d1b3b6039c06bcf17b38f788bb
LeXuanHoangNam <lenam7546@gmail.com>
2025-06-04 16:40:30 +0700

    ok

--- satrect/src/main/java/com/example/satrect/configuration/CorsConfig.java
+++ satrect/src/main/java/com/example/satrect/configuration/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
