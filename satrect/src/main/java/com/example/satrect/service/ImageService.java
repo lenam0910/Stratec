d41d789ca9ebb9e87937405f6d9938c293f7b270
quyen <nnguyenvanquyen2002@gmail.com>
2025-06-09 16:23:26 +0700

    update @transacton for imageService

--- satrect/src/main/java/com/example/satrect/service/ImageService.java
+++ satrect/src/main/java/com/example/satrect/service/ImageService.java
@@ -2,6 +2,7 @@ package com.example.satrect.service;
 
 import java.util.List;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +12,7 @@ import com.example.satrect.dto.response.ImageResponse;
 import com.example.satrect.entity.Image;
 
 public interface ImageService {
+    @Transactional
     ImageResponse postImage(MultipartFile imagePath, String imageName);
 
     ImageResponse getImageById(String imageId);
